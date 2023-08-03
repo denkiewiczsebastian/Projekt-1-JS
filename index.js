7c3434dafef5a389ae1c56e8e0b1a6ce3e74724d
Sebastian Denkiewicz <denkiewiczsebastian@gmail.com>
2023-08-03 23:39:15 +0200

    project done

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const incomeForm = document.getElementById('incomeForm');
-const incomeNameInput = document.getElementById('incomeName');
-const incomeAmountInput = document.getElementById('incomeAmount');
-const incomeList = document.querySelector('.incomeList');
+const incomeForm = document.getElementById("incomeForm");
+const incomeNameInput = document.getElementById("incomeName");
+const incomeAmountInput = document.getElementById("incomeAmount");
+const incomeList = document.querySelector(".incomeList");
 
-const expenseForm = document.getElementById('expenseForm');
-const expenseNameInput = document.getElementById('expenseName');
-const expenseAmountInput = document.getElementById('expenseAmount');
-const expenseList = document.querySelector('.expenseList');
+const expenseForm = document.getElementById("expenseForm");
+const expenseNameInput = document.getElementById("expenseName");
+const expenseAmountInput = document.getElementById("expenseAmount");
+const expenseList = document.querySelector(".expenseList");
 
-const balanceDiv = document.querySelector('.balance');
+const balanceDiv = document.querySelector(".balance");
 
 let incomes = [];
 let expenses = [];
 
 function updateBalance() {
   const totalIncome = incomes.reduce((acc, income) => acc + income.amount, 0);
-  const totalExpense = expenses.reduce((acc, expense) => acc + expense.amount, 0);
+  const totalExpense = expenses.reduce(
+    (acc, expense) => acc + expense.amount,
+    0
+  );
   const balance = totalIncome - totalExpense;
 
   if (balance > 0) {
     balanceDiv.textContent = `Możesz jeszcze wydać ${balance} złotych`;
-    balanceDiv.style.color = 'green';
+    balanceDiv.style.color = "green";
   } else if (balance === 0) {
-    balanceDiv.textContent = 'Bilans wynosi zero';
-    balanceDiv.style.color = 'black';
+    balanceDiv.textContent = "Bilans wynosi zero";
+    balanceDiv.style.color = "black";
   } else {
-    balanceDiv.textContent = `Bilans jest ujemny. Jesteś na minusie ${Math.abs(balance)} złotych`;
-    balanceDiv.style.color = 'red';
+    balanceDiv.textContent = `Bilans jest ujemny. Jesteś na minusie ${Math.abs(
+      balance
+    )} złotych`;
+    balanceDiv.style.color = "red";
   }
 }
 
@@ -35,15 +40,15 @@ function addIncome(e) {
   const name = incomeNameInput.value.trim();
   const amount = parseFloat(incomeAmountInput.value);
 
-  if (name === '' || isNaN(amount)) {
+  if (name === "" || isNaN(amount)) {
     return;
   }
 
   const income = { name, amount };
   incomes.push(income);
-  incomeList.appendChild(createListItem(income, 'income'));
-  incomeNameInput.value = '';
-  incomeAmountInput.value = '';
+  incomeList.appendChild(createListItem(income, "income", incomes.length - 1));
+  incomeNameInput.value = "";
+  incomeAmountInput.value = "";
   updateBalance();
 }
 
@@ -52,92 +57,86 @@ function addExpense(e) {
   const name = expenseNameInput.value.trim();
   const amount = parseFloat(expenseAmountInput.value);
 
-  if (name === '' || isNaN(amount)) {
+  if (name === "" || isNaN(amount)) {
     return;
   }
 
   const expense = { name, amount };
   expenses.push(expense);
-  expenseList.appendChild(createListItem(expense, 'expense'));
-  expenseNameInput.value = '';
-  expenseAmountInput.value = '';
+  expenseList.appendChild(
+    createListItem(expense, "expense", expenses.length - 1)
+  );
+  expenseNameInput.value = "";
+  expenseAmountInput.value = "";
   updateBalance();
 }
 
-function createListItem(item, type) {
-  const li = document.createElement('li');
+function createListItem(item, type, id) {
+  const li = document.createElement("li");
   li.textContent = `${item.name}: ${item.amount} zł`;
-  li.style.color = type === 'income' ? 'green' : 'red';
+  li.style.color = type === "income" ? "green" : "red";
+  li.setAttribute("data-id", id);
 
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Usuń';
-  deleteButton.addEventListener('click', () => deleteItem(item, type));
+  li.appendChild(createEditButton(id, type));
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Usuń";
+  deleteButton.addEventListener("click", () => deleteItem(id, type));
   li.appendChild(deleteButton);
 
   return li;
 }
 
-function deleteItem(item, type) {
-  if (type === 'income') {
-    incomes = incomes.filter(income => income !== item);
-    incomeList.removeChild(item.parentNode);
+function deleteItem(id, type) {
+  if (type === "income") {
+    incomes.splice(id, 1);
   } else {
-    expenses = expenses.filter(expense => expense !== item);
-    expenseList.removeChild(item.parentNode);
+    expenses.splice(id, 1);
   }
+
+  updateList();
+}
+
+function editItem(id, type) {
+  const newName = prompt("Podaj nową nazwę:");
+  const newAmount = parseFloat(prompt("Podaj nową kwotę:"));
+
+  if (newName === null || newName.trim() === "" || isNaN(newAmount)) {
+    return;
+  }
+
+  if (type === "income") {
+    incomes[id].name = newName;
+    incomes[id].amount = newAmount;
+  } else {
+    expenses[id].name = newName;
+    expenses[id].amount = newAmount;
+  }
+
+  updateList();
+}
+
+function createEditButton(id, type) {
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edytuj";
+  editButton.addEventListener("click", () => editItem(id, type));
+  return editButton;
+}
+
+function updateList() {
+  incomeList.innerHTML = "";
+  expenseList.innerHTML = "";
+
+  for (let i = 0; i < incomes.length; i++) {
+    incomeList.appendChild(createListItem(incomes[i], "income", i));
+  }
+
+  for (let i = 0; i < expenses.length; i++) {
+    expenseList.appendChild(createListItem(expenses[i], "expense", i));
+  }
+
   updateBalance();
 }
-// ... (wcześniejsza część skryptu)
 
-function editItem(item, type) {
-    const newName = prompt('Podaj nową nazwę:', item.name);
-    const newAmount = parseFloat(prompt('Podaj nową kwotę:', item.amount));
-  
-    if (newName === null || newName.trim() === '' || isNaN(newAmount)) {
-      return;
-    }
-  
-    const newItem = { name: newName, amount: newAmount };
-  
-    if (type === 'income') {
-      incomes = incomes.map(income => (income === item ? newItem : income));
-      const incomeListItem = item.parentNode;
-      incomeListItem.textContent = `${newName}: ${newAmount} zł`;
-      incomeListItem.appendChild(createEditButton(newItem, 'income'));
-    } else {
-      expenses = expenses.map(expense => (expense === item ? newItem : expense));
-      const expenseListItem = item.parentNode;
-      expenseListItem.textContent = `${newName}: ${newAmount} zł`;
-      expenseListItem.appendChild(createEditButton(newItem, 'expense'));
-    }
-    updateBalance();
-  }
-  
-  function createEditButton(item, type) {
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Edytuj';
-    editButton.addEventListener('click', () => editItem(item, type));
-    return editButton;
-  }
-  
-  function createListItem(item, type) {
-    const li = document.createElement('li');
-    li.textContent = `${item.name}: ${item.amount} zł`;
-    li.style.color = type === 'income' ? 'green' : 'red';
-  
-    li.appendChild(createEditButton(item, type));
-  
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Usuń';
-    deleteButton.addEventListener('click', () => deleteItem(item, type));
-    li.appendChild(deleteButton);
-  
-    return li;
-  }
-  
-  // ... (wcześniejsza część skryptu)
-  
-  
-
-incomeForm.addEventListener('submit', addIncome);
-expenseForm.addEventListener('submit', addExpense);
+incomeForm.addEventListener("submit", addIncome);
+expenseForm.addEventListener("submit", addExpense);
